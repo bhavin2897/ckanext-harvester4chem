cb00c0e7d8a0a5a39787aaa741e93bf529c5559a
Bhavin Katabathuni <bhavin2897@gmail.com>
2024-08-21 11:15:56 +0200

    newer version for all harvesters

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.0.1',
+    version='0.1.0',
 
     description='''Append all the harvesters in Search Service NFDI4Chem''',
     long_description=long_description,
@@ -31,23 +31,6 @@ setup(
 
     # Choose your license
     license='AGPL',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        # 3 - Alpha
-        # 4 - Beta
-        # 5 - Production/Stable
-        'Development Status :: 4 - Beta',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
-    ],
-
 
     # What does your project relate to?
     keywords='''CKAN harvesting metadata NFDI4Chem OAI-PMH Dublin Core Schemaorg Bioschemas''',
@@ -72,24 +55,24 @@ setup(
     package_data={
     },
 
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages.
-    # see http://docs.python.org/3.4/distutils/setupscript.html
-    # installing-additional-files
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points='''
         [ckan.plugins]
-        harvester4chem=ckanext.harvester4chem.plugin:Harvester4ChemPlugin
-
+        
+        bioschemaharvester=     ckanext.harvester4chem.harvester.bioschemascrap:BioSchemaMUHarvester
+        nmrxivharvester=        ckanext.harvester4chem.harvester.nmrXiv_harvester:NMRxIVBioSchema
+        chemotionharvester=     ckanext.harvester4chem.harvester.chemotion_repo:ChemotionRepoHarvester
+        oaipmh_harvester=       ckanext.harvester4chem.harvester:oaipmh:OaipmhHarvester
+        oaipmh_dc_harvester=    ckanext.harvester4chem.harvester:oaipmh_dc:OaipmhDCHarvester
+        
         [babel.extractors]
         ckan = ckan.lib.extract:extract_ckan
     ''',
 
+    # harvester4chem=         ckanext.harvester4chem.plugin:Harvester4ChemPlugin
     # If you are changing from the default layout of your extension, you may
     # have to change the message extractors, you can read more about babel
     # message extraction at
